2f6cb7c1f23863bceb16874fcbe3f454dac1b34d
Alvie Rahman <alvierahman90@gmail.com>
2020-02-22 14:10:09 +0000

    Fuzz location when reported by device

--- backend-php/api/post.php
+++ backend-php/api/post.php
@@ -7,6 +7,12 @@
 include("../include/inc.php");
 header("X-Hauk-Version: ".BACKEND_VERSION);
 
+function random_0_1() 
+{
+	return ((float)rand()*2 / (float)getrandmax()) -1.0;
+}
+
+
 requirePOST(
     "lat",  // Current latitude.
     "lon",  // Current longitude.
@@ -22,16 +28,26 @@ $session = new Client($memcache, $sid);
 if (!$session->exists()) die($LANG['session_expired']."\n");
 
 if (!$session->isEncrypted()) {
+    // it might actually be diameter not radius i'm not sure
+    $fuzzing_radius_metres = 50.0;
+    $fuzzing_radius = (0.00044962/50)*$fuzzing_radius_metres ; // 0.00044... is 50 metres
+    $vertical_change = $fuzzing_radius * random_0_1();
+    $horizontal_change = $fuzzing_radius * random_0_1();
+
     // Perform input validation.
-    $lat = floatval($_POST["lat"]);
-    $lon = floatval($_POST["lon"]);
+    $lat = floatval($_POST["lat"]) + $vertical_change;
+    $lon = floatval($_POST["lon"]) + $horizontal_change;
     $time = floatval($_POST["time"]);
     if ($lat < -90 || $lat > 90 || $lon < -180 || $lon > 180) die($LANG['location_invalid']."\n");
 
     // Not all devices report speed and accuracy, but if available, report them
     // too.
     $speed = isset($_POST["spd"]) ? floatval($_POST["spd"]) : null;
-    $accuracy = isset($_POST["acc"]) ? floatval($_POST["acc"]) : null;
+    // choose whichever is the least accurate - the real location or the fuzzed location
+    $accuracy = isset($_POST["acc"])
+	    	? $fuzzing_radius_metres*2
+	    	: floatval($_POST["acc"])
+	    : null;
     $provider = isset($_POST["prv"]) && $_POST["prv"] == "1" ? 1 : 0;
 
     // The location data object contains the sharing interval (i), duration (d)
